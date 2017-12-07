3231b9c8d25a2d74407be678891038040dc86220
Alexey <shulzhenkoalexey@gmail.com>
2017-12-08 00:36:05 +0200

    edited setup.bat

--- proxy/setup.bat
+++ proxy/setup.bat
@@ -2,4 +2,4 @@
 cls
 C:\Users\Administrator\Downloads\nuget restore proxy.sln
 
-"C:\Windows\Microsoft.NET\Framework64\v2.0.50727\MSBuild.exe" proxy.sln
+"C:\Windows\Microsoft.NET\Framework64\v3.5\MSBuild.exe" proxy.sln
