3240e23b878ee9744d1e801f12ee99d8c46bdc55
Alexey <shulzhenkoalexey@gmail.com>
2017-12-07 23:38:09 +0200

    try no3

--- proxy/setup.bat
+++ proxy/setup.bat
@@ -2,7 +2,4 @@
 cls
 SET var=%cd%
 cd C:\Users\Administrator\Downloads
-nuget restore $Source.tti_global_repo$\proxy\proxy.sln
-
-cd C:\Windows\Microsoft.NET\Framework\v4.0.30319\
-msbuild $Source.tti_global_repo$\proxy\proxy.sln
+nuget restore %cd%\proxy\proxy.sln
