0e8071f436f4d356249af7ec0b1ee7137d7544b3
Вадим Томащук < >
2017-12-13 07:40:20 +0200

    Added Daily Timelogs

--- clients/mobile/components/Timelogs.js
+++ clients/mobile/components/Timelogs.js
@@ -1,15 +1,84 @@
 import React, { Component } from 'react'
-import { View, Text} from 'react-native'
+import { View, Text, Dimensions} from 'react-native'
 import PropTypes from 'prop-types';
 
 export default class Timelogs extends React.Component {
 	render() {
 		return (
-			<View>
-				{this.props.timelogs.map((timelog, i) =>
-					<Text key={i}>{timelog.id}</Text>
-				)}
-			</View>
+		    <View>
+			<View style={styles.containerTimelogs}>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                    <View style={{flex:1.1}}>
+                        <View style={styles.activePanelTimelog}>
+                            <Text style={styles.activeTitleOption}>Today</Text>
+                        </View>
+					</View>
+                    <View style={styles.panel}>
+                        <View style={styles.panelTimelog}>
+                            <Text style={styles.titleOption}>Weekly</Text>
+                        </View>
+                    </View>
+                    <View style={styles.panel}>
+                        <View style={styles.panelTimelog}>
+                            <Text style={styles.titleOption}>Stats</Text>
+                        </View>
+                    </View>
+                    <View style={styles.panel}>
+                        <View style={styles.panelTimelog}>
+                            <Text style={styles.titleOption}>Filters</Text>
+                        </View>
+                    </View>
+				</View>
+            </View>
+
+
+            <View style={styles.tasksAndTimes}>
+				<View style={styles.containerTask}>
+                    <View style={{flex: 1.1, flexDirection: 'row', backgroundColor: '#D3D3D3'}}>
+                        <View style={styles.timeCell}>
+                            <Text style={styles.timeFromTo}>18:30</Text>
+                            <Text style={styles.timeFromTo}>20:40</Text>
+                        </View>
+                        <Text style={styles.taskTitleTimelog}>Add task to Newton Extranet</Text>
+                        <View>
+                            <Text style={styles.hoursTimelog}>2h 10m</Text>
+                            <Text style={styles.projectTitleTimelog}>PROJECT NAME</Text>
+                        </View>
+                        </View>
+                </View>
+                <View style={styles.containerTask}>
+                    <View style={{flex: 1.1, flexDirection: 'row', backgroundColor: '#D3D3D3'}}>
+                        <View style={styles.timeCell}>
+                            <Text style={styles.timeFromTo}>18:30</Text>
+                            <Text style={styles.timeFromTo}>20:40</Text>
+                        </View>
+                        <Text style={styles.taskTitleTimelog}>Add task to Newton Extranet</Text>
+                        <View>
+                            <Text style={styles.hoursTimelog}>2h 10m</Text>
+                            <Text style={styles.projectTitleTimelog}>PROJECT NAME</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.containerTask}>
+                    <View style={{flex: 1.1, flexDirection: 'row', backgroundColor: '#D3D3D3'}}>
+                        <View style={styles.timeCell}>
+                            <Text style={styles.timeFromTo}>18:30</Text>
+                            <Text style={styles.timeFromTo}>20:40</Text>
+                        </View>
+                        <Text style={styles.taskTitleTimelog}>Add task to Newton Extranet</Text>
+                        <View>
+                            <Text style={styles.hoursTimelog}>2h 10m</Text>
+                            <Text style={styles.projectTitleTimelog}>PROJECT NAME</Text>
+                        </View>
+                    </View>
+                </View>
+
+                    <Text style={styles.totalTime}>Total TI:ME</Text>
+                    {/*<Text style={styles.totalTime}> </Text>*/}
+
+
+            </View>
+            </View>
 		)
 	}
 }
--- clients/mobile/resources/styles.js
+++ clients/mobile/resources/styles.js
@@ -36,14 +36,16 @@ export default styles = StyleSheet.create({
     },
     totalTime: {
         backgroundColor: '#fff',
-        fontSize: 24,
+        fontSize: 30,
         fontWeight: 'bold',
-        width: 100,
-        marginLeft: 20,
+        paddingTop: 15,
+        marginRight: 20,
+        textAlign: 'right',
     },
     buttonBackground: {
         flex: 0.15,
         alignItems: 'center',
+        backgroundColor: '#fff',
         padding: 10
     },
     saveButton: {
@@ -54,130 +56,75 @@ export default styles = StyleSheet.create({
         height: 50
     },
 
-
-    wrapper:{
-        flex:1,
+    panel: {
+       flex: 1.1,
     },
-
-    logoContainer: {
-        alignItems: 'center',
-        flexGrow: 1,
-        justifyContent: 'center',
+    panelTimelog: {
+        backgroundColor: '#fff',
+        borderWidth:0.1,
+        height: 30,
+        // paddingTop: 50
     },
-
-    logoName:{
-        color: '#A52A2A',
-        fontFamily: 'serif',
-        lineHeight: 48,
+    activePanelTimelog: {
+        backgroundColor: '#c0c0c0',
+        borderWidth:0.1,
+        height: 30
+        // marginTop: 50
+    },
+    titleOption: {
         fontSize: 20,
         fontWeight: 'bold',
-        justifyContent: 'center',
-        alignItems: 'center',
-        bottom: 40,
-    },
-
-    inputContainer: {
-        padding: 20,
-        bottom: 60,
-    },
-
-    input: {
-        position: 'relative',
-        height: 50,
-        width: 320,
-        backgroundColor: '#f6f6f6',
-        elevation: 3,
-        marginBottom: 20,
-        color: '#7e7e7e',
-        borderRadius: 12,
-        paddingHorizontal: 10,
-    },
-    inputText:{
-        fontSize: 20,
-        color: '#2980b9',
-        fontWeight: '900',
-    },
-
-    signInButtonContainer: {
-        backgroundColor: '#191970',
-        paddingVertical: 15,
-        width: 200,
-        left: 50,
-        justifyContent: 'center',
-    },
-    signInButtonText: {
-        fontSize: 20,
         textAlign: 'center',
-        color: '#FFFFFF',
-        fontWeight: '700',
+        // marginLeft: 20,
     },
-
-    header:{
-        flexDirection: 'row',
-        backgroundColor: '#90a6da',
-        flex: 0.3,
-        height: 100,
-        padding: 12,
+    activeTitleOption: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        // marginLeft: 20,
+        color: 'white',
+        textAlign: 'center'
     },
-
-    footerWhite:{
-        position: 'absolute',
-        flex:0.1,
-        left: 0,
-        right: 0,
-        bottom: -10,
-        backgroundColor:'#FFFAFA',
-        flexDirection:'row',
-        height:80,
-        alignItems:'center',
-        justifyContent: 'center',
+    containerTask: {
+        backgroundColor: '#D3D3D3',
+        width: 360,
+        height: 70,
+        marginLeft: 10,
+        marginTop:12,
+        // flex: 1,
+        // flexDirection: 'column',
+        justifyContent: 'flex-start',
+    },
+    timeFromTo: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        backgroundColor: 'white',
+        textAlign: 'center',
 
     },
-
-    footerBrown:{
-        position: 'absolute',
-        flex:0.1,
-        left: 0,
-        right: 0,
-        bottom: -10,
-        backgroundColor:'#A52A2A',
-        flexDirection:'row',
-        height:80,
-        alignItems:'center',
-        justifyContent: 'center',
+    timeCell: {
+        width: 60,
+        marginTop: 10,
+        marginLeft: 8,
     },
-
-    footerPrPol:{
-        position: 'absolute',
-        top: 11,
-        fontSize: 15,
-        color: '#191970',
-        alignItems:'center',
-    },
-
-    newIdeaCorpLog: {
-        position: 'absolute',
-        fontSize: 12,
-        color: '#DCDCDC',
-        alignItems:'center',
-    },
-
-    taskContainer:{
+    containerTimelogs: {
         flex: 1,
+        flexDirection: 'column'
     },
-
-    taskTitle:{
-        backgroundColor: '#FFFFFF',
-        fontSize: 15,
-        left: '7%',
-        textAlign: 'center',
-        borderRadius: 20,
+    hoursTimelog: {
+        paddingTop: 8,
+        textAlign: 'right',
+        fontSize: 28,
     },
-
-    timeShow:{
-        fontSize: 40,
-        left: '22%',
-        color: '#FFFFFF',
-    }
-
+    projectTitleTimelog: {
+        paddingTop: 5,
+    },
+    taskTitleTimelog: {
+        fontSize: 12,
+        width: 160,
+        paddingTop: 14,
+        paddingLeft: 10
+    },
+    tasksAndTimes: {
+        paddingTop: 40,
+    },
 });
