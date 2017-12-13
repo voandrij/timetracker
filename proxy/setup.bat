10f28c876cea71f9491abbd89c409bfc758ff3dc
Alexey <shulzhenkoalexey@gmail.com>
2017-12-13 07:43:59 +0200

    Removed one level from path to executable in .bat

--- proxy/setup.bat
+++ proxy/setup.bat
@@ -2,5 +2,5 @@
 cls
 rem C:\Users\Administrator\Downloads\nuget restore proxy.sln
 rem "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin\MSBuild.exe" proxy.sln
-cd proxy/proxy
+cd proxy
 dotnet run
