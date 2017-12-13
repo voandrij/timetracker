aa3fe362b528c5daf67d193566c50c2cb32cbfc3
Alexey <shulzhenkoalexey@gmail.com>
2017-12-13 07:41:28 +0200

    Added run command to .bat file

--- proxy/setup.bat
+++ proxy/setup.bat
@@ -1,4 +1,6 @@
 @echo off
 cls
-C:\Users\Administrator\Downloads\nuget restore proxy.sln
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin\MSBuild.exe" proxy.sln
+rem C:\Users\Administrator\Downloads\nuget restore proxy.sln
+rem "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin\MSBuild.exe" proxy.sln
+cd proxy/proxy
+dotnet run
